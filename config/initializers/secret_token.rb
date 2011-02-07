e4d34802c5d2ee51eae6fdda1a97132b543b3bc2
Mikel Lindsaar <mikel@rubyx.com>
2011-02-07 20:14:37 +0800

    Base server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRubyCasClient::Application.config.secret_token = 'fa227734ca3fd6f91079106a9ef734c9753af9512263a74cf01f0d576da266294a2530a6f36701b2ccf8ed9d4575cc41191c8debca853dd0c0384d5dc084eb67'
